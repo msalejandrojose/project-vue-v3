1579a456e6bef07fe4b18d9fc0ce610873ce52b4
msalejandrojose <msalejandrojose7@gmail.com>
2023-05-07 13:04:51 +0200

    include docker files

---
+++ docker/run-stack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export $(cat ../.env | grep '^PROJECT_ID=' | xargs)
+
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker compose --env-file ../.env -f ./docker-compose.yml down
+docker compose --env-file ../.env -f ./docker-compose.yml build --no-cache --build-arg mode=local --build-arg project_id=$PROJECT_ID
+docker compose --env-file ../.env -f ./docker-compose.yml up -d
+
+docker ps -a
+sleep 5s
