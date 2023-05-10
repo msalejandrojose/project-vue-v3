090dca6d53959f51303c73f4e07512f046d89d74
msalejandrojose <msalejandrojose7@gmail.com>
2023-05-11 00:18:56 +0200

    create container db

--- docker/run-stack.sh
+++ docker/run-stack.sh
@@ -5,7 +5,7 @@ export $(cat ../.env | grep '^PROJECT_ID=' | xargs)
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker compose --env-file ../.env -f ./docker-compose.yml down
-docker compose --env-file ../.env -f ./docker-compose.yml build --no-cache --build-arg mode=local --build-arg project_id=$PROJECT_ID
+docker compose --env-file ../.env -f ./docker-compose.yml build --build-arg mode=local --build-arg project_id=$PROJECT_ID
 docker compose --env-file ../.env -f ./docker-compose.yml up -d
 
 docker ps -a
